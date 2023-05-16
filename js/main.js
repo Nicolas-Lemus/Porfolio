f4266c6abdea6089b2ea0cca0e3f60849aed7231
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-05-16 00:21:03 -0300

    Realizando media queries

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const imagen =document.querySelector("#imagen");
+imagen.addEventListener("click",()=>{
+    Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: "imageUrl",
+        imageWidth: 500,
+        imageHeight: 500,
+        imageAlt: 'Custom image',
+    })
+});
